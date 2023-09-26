11fd94586734163f25dc01a3fc9b1d2eeb65a5a7
MOHAMED Ibrahim <mohamed.ibm@hotmail.fr>
2023-09-26 15:09:13 +0200

    add signin and signup form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { DestinationComponent } from './destination/destination.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
-{path: '', component: HomepageComponent},
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: 'accueil', component: HomepageComponent },
+  { path: 'destinations', component: DestinationComponent },
+  { path: 'connexion', component: LoginComponent },
+  { path: 'contact', component: ContactUsComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
