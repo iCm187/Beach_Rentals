24e4371b1abc73ffa0c0c2cfdb0260ac2020ecbe
MOHAMED Ibrahim <mohamed.ibm@hotmail.fr>
2023-11-20 12:09:31 +0100

    add page profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      motDePasse: ['', [Validators.required, Validators.minLength(6)]],
+      confirmationMotDePasse: ['', Validators.required],
+      adresseDeResidence: ['', Validators.required],
+      moyenDePaiement: ['', Validators.required],
+      departementVille: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.profileForm.valid) {
+      console.log(this.profileForm.value);
+      // Here you would typically send the form data to a server
+    }
+  }
 }
